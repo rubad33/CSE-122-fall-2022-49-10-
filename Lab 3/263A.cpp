4527bba1a3b2e6b0250ded1d34a7356bfac895b1
Syed Rubad <109476208+rubad33@users.noreply.github.com>
2022-08-01 21:51:32 +0600

    Add files via upload

---
+++ Lab 3/263A.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+int w,ar[10][10],i,j,x,y;
+for(i=1;i<=5;i++){
+    for(j=1;j<=5;j++){
+       cin>>ar[i][j];
+    }
+}
+for(i=1;i<=5;i++){
+    for(j=1;j<=5;j++){
+       if(ar[i][j]==1){
+        x=i;
+        y=j;
+       }
+       }
+    }
+w=abs(3-x)+abs(3-y);
+cout <<w;
+}
