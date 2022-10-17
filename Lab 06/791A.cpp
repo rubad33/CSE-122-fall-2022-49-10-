a8c34f3cb060abd9c939014d09259907453aac37
Syed Rubad <109476208+rubad33@users.noreply.github.com>
2022-10-17 23:33:45 +0600

    Add files via upload

---
+++ Lab 06/791A.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+int i,a,b,x=0,y=0;
+cin>>a>>b;
+for(i=1;i<=10;i++){
+    a=a*3;
+    b=b*2;
+    if(a>b){
+        cout<<i;
+        break;
+    }
+}
+
+return 0;
+}
