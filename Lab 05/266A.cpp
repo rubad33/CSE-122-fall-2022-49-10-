119e67cee5f9a82d3b66139731d7b35253a43a7c
Syed Rubad <109476208+rubad33@users.noreply.github.com>
2022-10-17 23:24:57 +0600

    Add files via upload

---
+++ Lab 05/266A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,i,coun=0;
+cin>>a;
+string s;
+cin>>s;
+for(i=0;i<a;i++){
+    if(s[i]==s[i+1]){
+        coun++;
+    }
+}
+cout<<coun;
+return 0;
+}
